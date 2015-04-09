d18661c94e55ced73c3cc411b2b44f2e010c6852
hjkk1987 <hjkk19872@163.com>
2015-04-09 09:33:50 +0800

    Signed-off-by: hjkk1987 <hjkk19872@163.com>

---
+++ src/com/push/demo/main/MainAty.java
@@ -0,0 +1,38 @@
+package com.push.demo.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainAty extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+}
