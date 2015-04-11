113f85fff6351451d78ed7161be290bc13e6134b
hjkk1987 <hjkk19872@163.com>
2015-04-11 14:32:38 +0800

    Signed-off-by: hjkk1987 <hjkk19872@163.com>

--- src/com/push/demo/main/MainAty.java
+++ src/com/push/demo/main/MainAty.java
@@ -1,23 +1,16 @@
 package com.push.demo.main;
 
-
-import com.push.demo.app.Services;
-import com.push.demo.app.Services.ServiceBinder;
 import com.push.demo.device.Bluetooth;
 import com.push.demo.device.Wifi;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 
 public class MainAty extends Activity {
 	private Wifi mWifi = null;
-	private Services mServices = null;
+
 	private Bluetooth mBluetooth = null;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +19,11 @@ public class MainAty extends Activity {
 
 		setContentView(R.layout.activity_main);
 		mWifi = new Wifi();
-		Intent intent = new Intent(MainAty.this, Services.class);
-		bindService(intent, mConnect, Context.BIND_AUTO_CREATE);
-
 
 		//ÃÌº”¿∂—¿œÓƒø
 		mBluetooth = new Bluetooth();
+
 	}
-
-	private ServiceConnection mConnect = new ServiceConnection() {
-
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			// TODO Auto-generated method stub
-			mServices = null;
-		}
-
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			// TODO Auto-generated method stub
-			ServiceBinder Ibinder = (ServiceBinder) service;
-			mServices = Ibinder.getService();
-
-		}
-	};
 
 	@Override
 	protected void onRestart() {
