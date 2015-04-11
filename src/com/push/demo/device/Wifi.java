ee5d2edd7de297c8dad2b2afe27429ab42735200
hjkk1987 <hjkk19872@163.com>
2015-04-11 16:04:14 +0800

    merge content from  branch master to branch0411 add function service

--- src/com/push/demo/device/Wifi.java
+++ src/com/push/demo/device/Wifi.java
@@ -13,3 +13,4 @@ public class Wifi {
 	}
 
 }
+
--- src/com/push/demo/main/MainAty.java
+++ src/com/push/demo/main/MainAty.java
@@ -1,16 +1,24 @@
 package com.push.demo.main;
 
+import com.push.demo.app.Services;
+import com.push.demo.app.Services.ServiceBinder;
 import com.push.demo.device.Bluetooth;
 import com.push.demo.device.Wifi;
 import com.push.demo.view.PlayInter;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 public class MainAty extends Activity {
 	private Wifi mWifi = null;
 	private Bluetooth mBluetooth = null;
 	private PlayInter mPlay = null;
+	private Services mServices = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +28,28 @@ public class MainAty extends Activity {
 		setContentView(R.layout.activity_main);
 		mWifi = new Wifi();
 
-		//添加蓝牙项目
+		// 添加蓝牙项目
 		mBluetooth = new Bluetooth();
 		mPlay = new PlayInter();
-
+		Intent intent = new Intent(MainAty.this, Services.class);
+		bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
 	}
+
+	ServiceConnection mServiceConnection = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			// TODO Auto-generated method stub
+			mServices = null;
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// TODO Auto-generated method stub
+			ServiceBinder mBinder = (ServiceBinder) service;
+			mServices = mBinder.getService();
+		}
+	};
 
 	@Override
 	protected void onRestart() {
