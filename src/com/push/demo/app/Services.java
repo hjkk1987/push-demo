37a4f363131b5689f414ddf51c8ce482daa71696
hjkk1987 <hjkk19872@163.com>
2015-04-11 14:08:36 +0800

    Signed-off-by: hjkk1987 <hjkk19872@163.com>

---
+++ src/com/push/demo/app/Services.java
@@ -0,0 +1,15 @@
+package com.push.demo.app;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Services extends Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
