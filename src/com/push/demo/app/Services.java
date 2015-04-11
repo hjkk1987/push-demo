46c8389a25c8a9952ffd670ae54b653e194c1626
hjkk1987 <hjkk19872@163.com>
2015-04-11 14:27:51 +0800

    Signed-off-by: hjkk1987 <hjkk19872@163.com>

--- src/com/push/demo/app/Services.java
+++ src/com/push/demo/app/Services.java
@@ -2,14 +2,30 @@ package com.push.demo.app;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class Services extends Service{
+	public  ServiceBinder mIBinder=new ServiceBinder();
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		
+	}
+	
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
-		return null;
+		return mIBinder;
 	}
+	
 
+	
+	public class ServiceBinder extends Binder {
+        public Services getService() {
+            return Services.this;
+        }
+}
 }
--- src/com/push/demo/main/MainAty.java
+++ src/com/push/demo/main/MainAty.java
@@ -1,12 +1,20 @@
 package com.push.demo.main;
 
+import com.push.demo.app.Services;
+import com.push.demo.app.Services.ServiceBinder;
 import com.push.demo.device.Wifi;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 public class MainAty extends Activity {
 	private Wifi mWifi = null;
+	private Services mServices = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +22,26 @@ public class MainAty extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mWifi = new Wifi(MainAty.this);
+		Intent intent = new Intent(MainAty.this, Services.class);
+		bindService(intent, mConnect, Context.BIND_AUTO_CREATE);
 	}
+
+	private ServiceConnection mConnect = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			// TODO Auto-generated method stub
+			mServices = null;
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// TODO Auto-generated method stub
+			ServiceBinder Ibinder = (ServiceBinder) service;
+			mServices = Ibinder.getService();
+
+		}
+	};
 
 	@Override
 	protected void onRestart() {
